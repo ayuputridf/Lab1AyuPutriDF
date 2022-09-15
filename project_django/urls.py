816e67c3068e5b6b984ca24e162794225cc451e9
Ayu Putri Dewi Fitriyani <ayuputridf2@.com>
2022-09-15 13:12:40 +0700

    Add Lab 2 into Lab 1

--- project_django/urls.py
+++ project_django/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from wishlist.views import show_xml #sesuaikan dengan nama fungsi yang dibuat
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('example_app.urls')),
     path('wishlist/', include('wishlist.urls')),
+    path('xml/', show_xml, name='show_xml'), #sesuaikan dengan nama fungsi yang dibuat
 
 ]
